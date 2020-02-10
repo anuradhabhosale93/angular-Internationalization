ea7dddebf56939d1f699ef878bcb20ea79a27cde
pratikgada <pratikgada48@gmail.com>
2020-02-10 18:14:40 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'testing-demo-inter';
-}
+import { Component, LOCALE_ID, Inject } from '@angular/core';
+
+@Component({  selector: 'app-root', 
+ templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css']})
+  
+  export class AppComponent { 
+     title = 'i18nDemo';  languageList = [  { code: 'en', label: 'English' },   { code: 'hi', label: 'हिंदी' },    { code: 'es', label: 'Espanol' }  ];
+       constructor(@Inject(LOCALE_ID) protected localeId: string) { }
+  }
